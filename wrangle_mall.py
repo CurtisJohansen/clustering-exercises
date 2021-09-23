972ea8f45c348e3838ce1db7b0204497ceb54a89
CurtisJohansen <curtis.d.johansen@gmail.com>
2021-09-23 16:31:53 -0400

    Initial Commit

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -10,6 +10,8 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from matplotlib import cm
+import seaborn as sns
 
 # Library dealing with NA values
 
@@ -19,6 +21,9 @@ from sklearn.impute import SimpleImputer
 from env import host, user, password
 import wrangle_mall
 
+# Imports
+
+from sklearn.cluster import KMeans
 
 from sklearn.model_selection import train_test_split
 import sklearn.preprocessing
@@ -305,3 +310,46 @@ def wrangle_mall_data():
     print(f'   train_scaled shape: {train_scaled.shape}')
     print(f'validate_scaled shape: {validate_scaled.shape}')
     print(f'    test_scaled shape: {test_scaled.shape}')
+    
+    #######################################################
+
+def viz_iris(iris, kmeans):
+    
+    centroids = np.array(iris.groupby('cluster')['petal_length', 'sepal_length'].mean())
+    cen_x = [i[0] for i in centroids]
+    cen_y = [i[1] for i in centroids]
+    # cen_x = [i[0] for i in kmeans.cluster_centers_]
+    # cen_y = [i[1] for i in kmeans.cluster_centers_]
+    iris['cen_x'] = iris.cluster.map({0:cen_x[0], 1:cen_x[1], 2:cen_x[2]})
+    iris['cen_y'] = iris.cluster.map({0:cen_y[0], 1:cen_y[1], 2:cen_y[2]})
+
+    colors = ['#DF2020','#2095DF', '#81DF20' ]
+    iris['c'] = iris.cluster.map({0:colors[0], 1:colors[1], 2:colors[2]})
+    #plot scatter chart for Actual species and those predicted by K - Means
+
+    #specify custom palette for sns scatterplot
+    colors1 = ['#2095DF','#81DF20' ,'#DF2020']
+    customPalette = sns.set_palette(sns.color_palette(colors1))
+
+    #plot the scatterplots
+
+    #Define figure (num of rows, columns and size)
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10,10))
+
+    # plot ax1 
+    ax1 = plt.subplot(2,1,1) 
+    sns.scatterplot(data = iris, x = 'petal_length', y = 'sepal_length', ax = ax1, hue = 'species', palette=customPalette)
+    plt.title('Actual Species')
+
+    #plot ax2
+    ax2 = plt.subplot(2,1,2) 
+    ax2.scatter(iris.petal_length, iris.sepal_length, c=iris.c, alpha = 0.6, s=10)
+    ax2.set(xlabel = 'petal_length', ylabel = 'sepal_length', title = 'K - Means')
+
+    # plot centroids on  ax2
+    ax2.scatter(cen_x, cen_y, marker='X', c=colors, s=200)
+    
+    
+    iris.drop(columns = ['cen_x', 'cen_y', 'c'], inplace = True)
+    plt.tight_layout()
+    plt.show()
